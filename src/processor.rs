320e3c8fc0e09be42f4a260e3606d01d5d56ecf2
Johan Geluk <johan@geluk.io>
2020-11-12 00:15:54 +0100

    Implement dropping scrolling subs

--- src/processor.rs
+++ src/processor.rs
@@ -33,18 +33,55 @@ fn insert_smoketest(mut subs: Vec<Subtitle>) -> Vec<Subtitle> {
 }
 
 fn strip_ads(subs: Vec<Subtitle>) -> Result<Vec<Subtitle>> {
+    struct Marker {
+        sub: Subtitle,
+        is_ad: bool,
+    }
+    fn is_subset(prev: &Marker, cur: &Marker) -> bool {
+        let prev = &prev.sub.text[0];
+        let cur = &cur.sub.text[0];
+
+        // The previous subtitle must contain the current subtitle,
+        // and the index of the current subtitle in the previous subtitle
+        // should be zero, for it to be considered to be a match.
+        // Additionally, the previous subtitle should be at most 2
+        // characters shorter than the current subtitle.
+        prev.find(cur).map_or(false, |i| i == 0)
+        && prev.len().saturating_sub(cur.len()) <= 2
+    }
+    
     let regexes = load_regex()?;
-    Ok(subs
+    let mut marked_subs: Vec<Marker> = subs
         .into_iter()
-        .filter(|s| !regexes.iter().any(|r| is_match(r, s)))
-        .collect())
+        .map(|sub| {
+            let is_ad = regexes.iter().any(|r| is_match(r, &sub));
+            Marker { sub, is_ad }
+        }).collect();
+    
+    let mut prv_mrk = None;
+    for marker in marked_subs.iter_mut().rev() {
+        prv_mrk = if marker.is_ad {
+            Some(marker)
+        } else if prv_mrk.as_ref().map_or(false, |p| is_subset(p, &marker)) {
+            marker.is_ad = true;
+            eprintln!("Matched subset (base: '{}', sub: '{}'", prv_mrk.unwrap().sub.text[0], marker.sub.text[0]);
+            Some(marker)
+        } else {
+            None
+        }
+    }
+    let filtered = marked_subs.into_iter()
+        .filter(|m| !m.is_ad)
+        .map(|m| m.sub)
+        .collect();
+    Ok(filtered)
 }
 
 fn is_match(regex: &Regex, subtitle: &Subtitle) -> bool {
     subtitle.text.iter().any(|line| {
         let mtch = regex.is_match(line);
         if mtch {
-            println!("Matched \"{}\" against /{}/", line, regex);
+            eprintln!("Matched \"{}\" against /{}/", line, regex);
         }
         mtch
     })
@@ -57,7 +94,7 @@ fn load_regex() -> Result<Vec<Regex>> {
         .split('\n')
         .map(|p| p.trim_start())
         .filter(|p| !p.is_empty() && !p.starts_with('#'));
-    println!("{:#?}", patterns.clone().collect::<Vec<&str>>());
+    eprintln!("{:#?}", patterns.clone().collect::<Vec<&str>>());
     patterns
         .map(|p| Regex::new(p).context("Invalid regex."))
         .collect()
